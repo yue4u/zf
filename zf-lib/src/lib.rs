97308f1b7cb9ae4069817dcdae95058848adb538
rainy-me <github@yue.coffee>
2022-04-21 17:23:39 +0900

    feat: init gdns

---
+++ zf-lib/src/lib.rs
@@ -0,0 +1,23 @@
+use gdnative::prelude::*;
+
+#[derive(NativeClass)]
+#[inherit(Node)]
+struct HelloWorld;
+
+#[methods]
+impl HelloWorld {
+    fn new(_owner: &Node) -> Self {
+        HelloWorld
+    }
+
+    #[export]
+    fn _ready(&self, _owner: &Node) {
+        godot_print!("hello, zf outside")
+    }
+}
+
+fn init(handle: InitHandle) {
+    handle.add_class::<HelloWorld>();
+}
+
+godot_init!(init);

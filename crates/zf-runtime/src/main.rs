664a98771e51cb2161ccdfe10a90c7450062c5e5
yue4u <github@yue.coffee>
2022-10-07 02:25:59 +0900

    feat: use WritePipe!

--- crates/zf-runtime/src/main.rs
+++ crates/zf-runtime/src/main.rs
@@ -15,6 +15,7 @@ fn main() -> Result<()> {
 
 #[test]
 fn sanity() -> anyhow::Result<()> {
+    use wasi_common::pipe::WritePipe;
     use wasmtime::{Engine, Linker, Module, Store};
     use wasmtime_wasi::WasiCtxBuilder;
 
@@ -22,17 +23,26 @@ fn sanity() -> anyhow::Result<()> {
     let mut linker = Linker::new(&engine);
     wasmtime_wasi::add_to_linker(&mut linker, |s| s)?;
 
+    let stdout = WritePipe::new_in_memory();
+
     let wasi = WasiCtxBuilder::new()
-        .args(&["".to_owned(), "[1 2 3 ] | math sum".to_string()])?
+        .stdout(Box::new(stdout.clone()))
+        .args(&["".to_owned(), "[1 2 3] | math sum".to_string()])?
         .build();
     let mut store = Store::new(&engine, wasi);
 
-    let shell_wasm = include_bytes!("../../target/wasm32-wasi/debug/zf-shell.wasm");
+    let shell_wasm = include_bytes!("../../target/wasm32-wasi/release/zf-shell.wasm");
     let module = Module::from_binary(&engine, shell_wasm)?;
     linker.module(&mut store, "", &module)?;
     linker
         .get_default(&mut store, "")?
         .typed::<(), (), _>(&store)?
         .call(&mut store, ())?;
+
+    drop(store);
+
+    let contents: Vec<u8> = stdout.try_into_inner().unwrap().into_inner();
+
+    assert_eq!(String::from_utf8(contents).unwrap(), "6".to_string());
     Ok(())
 }

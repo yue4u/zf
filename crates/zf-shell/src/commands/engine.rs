a6328d040130e08b8daf9ad975472ccacfeb6666
yue4u <github@yue.coffee>
2022-10-27 17:29:36 +0900

    fix: engine cmd and queue free if hp < 0

--- crates/zf-shell/src/commands/engine.rs
+++ crates/zf-shell/src/commands/engine.rs
@@ -17,7 +17,7 @@ zf_call::empty_command!(
 
 zf_call::proxy_command!(
     EngineOn,
-    name: "engine start",
+    name: "engine on",
     usage: "Turn on engine",
     arg: CommandBridge::Engine(EngineCommand::On)
 );
@@ -56,10 +56,50 @@ impl Command for EngineThruster {
         call: &Call,
         _input: PipelineData,
     ) -> Result<PipelineData, ShellError> {
-        let t: f64 = call.req(engine_state, stack, 0)?;
-
-        let args = CommandBridge::Engine(EngineCommand::Thruster(t as i8));
-        imports::zf_call(args);
-        Ok(Value::Nothing { span: call.head }.into_pipeline_data())
+        thruster(engine_state, stack, call, _input)
     }
+}
+
+#[derive(Clone)]
+pub(crate) struct EngineThrusterShort;
+
+impl Command for EngineThrusterShort {
+    fn name(&self) -> &str {
+        "engine t"
+    }
+
+    fn signature(&self) -> nu_protocol::Signature {
+        Signature::build(self.name()).required(
+            "number", //
+            SyntaxShape::Int,
+            "engine thruster number to set",
+        )
+    }
+
+    fn usage(&self) -> &str {
+        "Set engine thruster"
+    }
+
+    fn run(
+        &self,
+        engine_state: &EngineState,
+        stack: &mut Stack,
+        call: &Call,
+        _input: PipelineData,
+    ) -> Result<PipelineData, ShellError> {
+        thruster(engine_state, stack, call, _input)
+    }
+}
+
+fn thruster(
+    engine_state: &EngineState,
+    stack: &mut Stack,
+    call: &Call,
+    _input: PipelineData,
+) -> Result<PipelineData, ShellError> {
+    let t: f64 = call.req(engine_state, stack, 0)?;
+
+    let args = CommandBridge::Engine(EngineCommand::Thruster(t as i8));
+    imports::zf_call(args);
+    Ok(Value::Nothing { span: call.head }.into_pipeline_data())
 }
--- crates/zf/src/units/t_dummy.rs
+++ crates/zf/src/units/t_dummy.rs
@@ -7,13 +7,17 @@ use gdnative::{
 #[inherit(Spatial)]
 pub struct TDummy {
     hp: Option<Ref<TextureProgress>>,
+    base_ref: Ref<Spatial>,
 }
 
 #[methods]
 impl TDummy {
-    fn new(_base: &Spatial) -> Self {
+    fn new(base: TRef<Spatial>) -> Self {
         godot_print!("prepare TDummy");
-        TDummy { hp: None }
+        TDummy {
+            hp: None,
+            base_ref: base.claim(),
+        }
     }
 
     #[method]
@@ -33,6 +37,10 @@ impl TDummy {
     #[method]
     pub fn damage(&self) {
         let hp = unsafe { self.hp.unwrap().assume_safe() };
-        hp.set_value(hp.value() - 0.5 * hp.max());
+        let hp_tmp = hp.value() - 0.5 * hp.max();
+        hp.set_value(hp_tmp);
+        if hp_tmp < 0. {
+            unsafe { self.base_ref.assume_safe() }.queue_free()
+        }
     }
 }

813efc98f633889e8b4d0bd0bf0ca1c97bc595c5
yue4u <github@yue.coffee>
2023-01-18 00:06:28 +0900

    try: fix look at again

--- crates/zf/src/weapons/homing_missile.rs
+++ crates/zf/src/weapons/homing_missile.rs
@@ -29,19 +29,18 @@ impl HomingMissile {
     #[method]
     fn _process(&self, #[base] base: &Spatial, delta: f32) {
         if let Some(local_target_pos) = self.target_pos {
-            // FIXME: better always use global transform?
+            // FIXME: better to always use global transform?
             let mut local_t = base.transform();
-            let global_t = base.global_transform();
+            local_t.origin = local_t.origin.move_toward(local_target_pos, delta * 20.);
 
-            let next_origin = local_t.origin.move_toward(local_target_pos, delta * 20.);
-
-            if local_t.origin.distance_to(local_target_pos) > 0.1 {
-                base.look_at(global_t.origin + next_origin, Vector3::UP);
-                base.rotate_object_local(Vector3::UP, PI)
-            }
-
-            local_t.origin = next_origin;
             base.set_transform(local_t);
+
+            if local_t.origin.distance_to(local_target_pos) > 0.01 {
+                base.look_at(local_t.origin.direction_to(local_target_pos), Vector3::UP);
+                base.rotate_object_local(Vector3::UP, PI)
+            } else {
+                base.queue_free()
+            }
         } else {
             base.translate(Vector3::new(0.0, 0.0, -delta * 300.0));
         }
